4bd7913721484d3782956d2cf3d9493f483d40d3
ianwookim <ianwookim@gmail.com>
2011-12-08 07:34:52 +0000

    add many functions.

--- csrc/pgslib.h
+++ csrc/pgslib.h
@@ -14,75 +14,93 @@ extern "C" {
   void FORTRANNAME(pgs_init_herwig)( void ) ;
   void FORTRANNAME(pgs_init_alpher)( void ) ; 
   //  void FORTRANNAME(pgs_herwig_susy_file)( ); 
-  //  int FORTRANNAME(pgs_random_seed)(); 
+  int  FORTRANNAME(pgs_random_seed)( int* ij ); 
   void FORTRANNAME(pgs_herwig_event)( void ); 
   void FORTRANNAME(pgs_alpher_event)( void ); 
-  //  logical FORTRANNAME(pgs_next_event)(); 
+  int  FORTRANNAME(pgs_next_event)( void );
   //  void FORTRANNAME(pgs_write_event)();
-  //  logical FORTRANNAME(pgs_read_event);
-  //  function pgs_ranmar(); 
-  //  subroutine pgs_rannor(a,b); 
+  int  FORTRANNAME(pgs_read_event)( void );
+  float FORTRANNAME(pgs_ranmar)( void ); 
+  void FORTRANNAME(pgs_rannor)( float* a, float* b); 
   //  subroutine RANMAR(); 
   void FORTRANNAME(pgs_trigger)(void); 
   void FORTRANNAME(pgs_find_photon_electron_trig)(void); 
   void FORTRANNAME(pgs_find_muon_trig)(void); 
   void FORTRANNAME(pgs_find_tau_trig)(void); 
-  //  subroutine pgs_trigger_cal_isosum(itrc,sum); 
-  //  subroutine pgs_track_assoc(eta,...); 
+  void FORTRANNAME(pgs_trigger_cal_isosum)(int* itrc,float* sum); 
+  
+  void FORTRANNAME(pgs_track_assoc)(float* eta, float* phi,
+                                    int* iseed, int* n10,
+                                    float* ptmax, int* nsio,
+                                    float* ptmaxiso); 
+  
   void FORTRANNAME(pgs_find_jet_trig)(void);
   void FORTRANNAME(pgs_find_vertex_trig)(void);
   void FORTRANNAME(pgs_find_met_trig)(void);
   void FORTRANNAME(pgs_find_tracks)(void);
-  // subroutine pgs_track_res(p,q,pnew,qnew);
+  //void FORTRANNAME(pgs_track_res)(double p[3], double* q,
+  //                                double pnew[3], double* qnew);
   void FORTRANNAME(pgs_fill_cal)(void); 
-  // logical function pgs_cal_crack(eta,phi);
+
+  int  FORTRANNAME(pgs_cal_crack)(double* eta,double* phi);
+
   void FORTRANNAME(pgs_find_trigger_clusters)(void); 
   void FORTRANNAME(pgs_cone_jets)(void);
   void FORTRANNAME(pgs_kt_jets)(void);
-  // real*7 function pgs_kt_dist(p1,...); 
+  // real*8 function pgs_kt_dist(p1,...); 
   void FORTRANNAME(pgs_recon)(void); 
   void FORTRANNAME(pgs_find_photon_electron)(void); 
-  // subroutine pgs_trk_isosum(eta,...); 
-  // subroutine pgs_eta_phi_hep_match(eta,...); 
-  // subroutine pgs_eta_phi_diff(...); 
-  // subroutine pgs_tower_isosum(...); 
+
+  void FORTRANNAME(pgs_trk_isosum)(double* eta, double* phi,
+                                   double* r, double* ptsum,
+                                   int* nsum, double* ptmax,
+                                   int* itrmax);
+  void FORTRANNAME(pgs_eta_phi_hep_match)(double* eta, double* phi,
+                                          double* ptcut, int* id,
+                                          int *msign, int* ind);
+  void FORTRANNAME(pgs_eta_phi_diff)(double* eta1, double* phi1,
+                                     double* eta2, double* phi2,
+                                     double* diff); 
+  void FORTRANNAME(pgs_tower_isosum)(int* ieta,int* iphi,double* sum); 
   void FORTRANNAME(pgs_find_muons)(void); 
   // real*8 function pgs_muo_eff ; 
   void FORTRANNAME(pgs_find_taus)(void);
   void FORTRANNAME(pgs_find_jets)(void);
-  // subroutine pgs_jet_flavor_tags(jet,zvertex);
-  // subroutine pgs_loose_btag(type,...); 
+  void FORTRANNAME(pgs_jet_flavor_tags)(int* jet,double* zvertex);
+  void FORTRANNAME(pgs_loose_btag)(int* type, int* c_in_cone, 
+                                   int* b_in_cone, double* etjin,
+                                   double* etaj, double* efftag); 
   // subroutine pgs_tight_btag(type,...); 
   // subroutine pgs_true_jet_type(jet...);
   // function pgs_cluster_width(iclu); 
   // function pgs_trigger_cluster_width(itrc); 
   void FORTRANNAME(pgs_find_heavy)(void); 
   // subroutine pgs_heavy_eff(v4,eff,trigeff);
-  // function et_obj(iobj);
-  // function pt_obj(iobj);
-  // function p_obj(iobj);
-  // function eta_obj(iobj); 
-  // function phi_obj(iobj); 
-  // function et_gen(ihep); 
-  // function pt_gen(ihep); 
-  // function p_gen(ihep); 
-  // function eta_gen(ihep); 
-  // function phi_gen(ihep); 
-  // function et_clu(iclu); 
-  // function pt_clu(iclu); 
-  // function p_clu(iclu); 
-  // function eta_clu(iclu); 
-  // function phi_clu(iclu); 
-  // function et_trc(itrc); 
-  // function pt_trc(itrc); 
-  // function p_trc(itrc); 
-  // function eta_trc(itrc); 
-  // function phi_trc(itrc); 
-  // function et_trk(itrk); 
-  // function pt_trk(itrk); 
-  // function p_trk(itrk); 
-  // function eta_trk(itrk); 
-  // function phi_trk(itrk); 
+  void FORTRANNAME(et_obj)(int* iobj);
+  void FORTRANNAME(pt_obj)(int* iobj);
+  void FORTRANNAME(p_obj)(int* iobj);  
+  void FORTRANNAME(eta_obj)(int* iobj);
+  void FORTRANNAME(phi_obj)(int* iobj);
+  void FORTRANNAME(et_gen)(int* ihep); 
+  void FORTRANNAME(pt_gen)(int* ihep); 
+  void FORTRANNAME(p_gen)(int* ihep); 
+  void FORTRANNAME(eta_gen)(int* ihep); 
+  void FORTRANNAME(phi_gen)(int* ihep); 
+  void FORTRANNAME(et_clu)(int* iclu); 
+  void FORTRANNAME(pt_clu)(int* iclu); 
+  void FORTRANNAME(p_clu)(int* iclu); 
+  void FORTRANNAME(eta_clu)(int* iclu); 
+  void FORTRANNAME(phi_clu)(int* iclu); 
+  void FORTRANNAME(et_trc)(int* itrc); 
+  void FORTRANNAME(pt_trc)(int* itrc); 
+  void FORTRANNAME(p_trc)(int* itrc); 
+  void FORTRANNAME(eta_trc)(int* itrc); 
+  void FORTRANNAME(phi_trc)(int* itrc); 
+  void FORTRANNAME(et_trk)(int* itrk); 
+  void FORTRANNAME(pt_trk)(int* itrk); 
+  void FORTRANNAME(p_trk)(int* itrk); 
+  void FORTRANNAME(eta_trk)(int* itrk); 
+  void FORTRANNAME(phi_trk)(int* itrk); 
   // real*8 function cos_theta(p,q); 
   // real*8 function cos_del_phi(p,q); 
   // real*8 function del_phi(p,q);
@@ -98,7 +116,7 @@ extern "C" {
   // subroutine pgs_dump_event(mask,hepcut,calcut); 
   void FORTRANNAME(pgs_cor_met)(void); 
   void FORTRANNAME(pgs_unique_id)(void);
-  // logical function pgs_object_cuts(iobj);
+  int  FORTRANNAME(pgs_object_cuts)(int* iobj);
   // subroutine pgs_tau_decay_mode(jtau...); 
 
 
--- lib/HEP/Package/PGS/PGSLIB/Internal.hsc
+++ lib/HEP/Package/PGS/PGSLIB/Internal.hsc
@@ -17,20 +17,30 @@ module HEP.Package.PGS.PGSLIB.Internal where
 #ccall pgs_init_herwig_ , IO ()
 #ccall pgs_init_alpher_ , IO ()
 
-#ccall pgs_herwig_event_ , IO ()
+
+#ccall pgs_random_seed_ , Ptr CInt -> IO CInt
+#ccall pgs_herwig_event_ , IO () 
 #ccall pgs_alpher_event_ , IO () 
+#ccall pgs_next_event_ , IO CInt 
+
+#ccall pgs_read_event_ , IO CInt
+#ccall pgs_ranmar_ , IO CFloat 
+#ccall pgs_rannor_ , Ptr CFloat -> Ptr CFloat -> IO ()
 
 #ccall pgs_trigger_ , IO ()
 #ccall pgs_find_photon_electron_trig_ , IO ()
 #ccall pgs_find_muon_trig_ , IO ()
 #ccall pgs_find_tau_trig_ , IO () 
-
+#ccall pgs_trigger_cal_isosum_ , Ptr CInt -> Ptr CFloat -> IO ()
+#ccall pgs_track_assoc_ , Ptr CFloat -> Ptr CFloat -> Ptr CInt -> Ptr CInt -> Ptr CFloat -> Ptr CInt -> Ptr CFloat -> IO ()
 #ccall pgs_find_jet_trig_ , IO ()
 #ccall pgs_find_vertex_trig_ , IO ()
 #ccall pgs_find_met_trig_ , IO ()
 #ccall pgs_find_tracks_ , IO ()
 
+
 #ccall pgs_fill_cal_ , IO ()
+#ccall pgs_cal_crack_ , Ptr CDouble -> Ptr CDouble -> IO CInt 
 
 #ccall pgs_find_trigger_clusters_ , IO ()
 #ccall pgs_cone_jets_ , IO ()
@@ -38,13 +48,49 @@ module HEP.Package.PGS.PGSLIB.Internal where
 
 #ccall pgs_recon_ , IO ()
 #ccall pgs_find_photon_electron_ , IO ()
-
+#ccall pgs_trk_isosum_ , Ptr CDouble -> Ptr CDouble -> Ptr CDouble -> Ptr CDouble -> Ptr CInt -> Ptr CDouble -> Ptr CInt -> IO ()
+#ccall pgs_eta_phi_hep_match_ , Ptr CDouble -> Ptr CDouble -> Ptr CDouble -> Ptr CInt -> Ptr CInt -> Ptr CInt -> IO ()
+#ccall pgs_eta_phi_diff_ , Ptr CDouble -> Ptr CDouble -> Ptr CDouble -> Ptr CDouble -> Ptr CDouble -> IO ()
+#ccall pgs_tower_isosum_ , Ptr CInt -> Ptr CInt -> Ptr CDouble -> IO ()
 #ccall pgs_find_muons_ , IO ()
+
+
 
 #ccall pgs_find_taus_ , IO ()
 #ccall pgs_find_jets_ , IO ()
+#ccall pgs_jet_flavor_tags_ , Ptr CInt -> Ptr CDouble -> IO ()
+#ccall pgs_loose_btag_ , Ptr CInt -> Ptr CInt -> Ptr CInt -> Ptr CDouble -> Ptr CDouble -> Ptr CDouble -> IO ()
+
 
 #ccall pgs_find_heavy_ , IO ()
 
+#ccall et_obj_ , Ptr CInt -> IO ()
+#ccall pt_obj_ , Ptr CInt -> IO ()
+#ccall p_obj_ , Ptr CInt -> IO ()
+#ccall eta_obj_ , Ptr CInt -> IO ()
+#ccall phi_obj_ , Ptr CInt -> IO ()
+#ccall et_gen_ , Ptr CInt -> IO ()
+#ccall pt_gen_ , Ptr CInt -> IO ()
+#ccall p_gen_ , Ptr CInt -> IO ()
+#ccall eta_gen_ , Ptr CInt -> IO ()
+#ccall phi_gen_ , Ptr CInt -> IO ()
+#ccall et_clu_ , Ptr CInt -> IO ()
+#ccall pt_clu_ , Ptr CInt -> IO ()
+#ccall p_clu_ , Ptr CInt -> IO ()
+#ccall eta_clu_ , Ptr CInt -> IO ()
+#ccall phi_clu_ , Ptr CInt -> IO ()
+#ccall et_trc_ , Ptr CInt -> IO ()
+#ccall pt_trc_ , Ptr CInt -> IO ()
+#ccall p_trc_ , Ptr CInt -> IO ()
+#ccall eta_trc_ , Ptr CInt -> IO ()
+#ccall phi_trc_ , Ptr CInt -> IO ()
+#ccall et_trk_ , Ptr CInt -> IO ()
+#ccall pt_trk_ , Ptr CInt -> IO ()
+#ccall p_trk_ , Ptr CInt -> IO ()
+#ccall eta_trk_ , Ptr CInt -> IO ()
+#ccall phi_trk_ , Ptr CInt -> IO ()
+
+
 #ccall pgs_cor_met_ , IO ()
 #ccall pgs_unique_id_ , IO ()
+#ccall pgs_object_cuts_ , Ptr CInt -> IO CInt 
